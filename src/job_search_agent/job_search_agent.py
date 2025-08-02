b470835b93f5c385c9b3571f7aa6c15d6e6cf3c7
Lukas Hamm <lukas@lukashamm.dev>
2025-08-02 16:16:39 +0800

    enable the llm model to use the search tool

--- src/job_search_agent/job_search_agent.py
+++ src/job_search_agent/job_search_agent.py
@@ -4,10 +4,13 @@ from web_search_agent import WebSearchTool
 
 config = Config()
 
-search = WebSearchTool(config)
+search = WebSearchTool(max_results=1)
 tools = [search] # Saving every tool in a list for later reference
 
 model = init_chat_model(config.llm_model, model_provider=config.model_provider, api_key=config.croq_api_key)
+model_with_tools = model.bind_tools(tools)
 
-response = model.invoke([{"role": "user", "content": "Hi, what can you do for me?"}])
-print(response.text())
+response = model_with_tools.invoke([{"role": "user", "content": "What remote jobs for Software Engineers in Australia are available?"}])
+
+print(f"Mesage content: {response.text()}\n")
+print(f"Tool calls: {response.tool_calls}")
--- src/job_search_agent/web_search_agent.py
+++ src/job_search_agent/web_search_agent.py
@@ -1,20 +1,28 @@
 from langchain_tavily import TavilySearch
 import json
 
-class WebSearchTool:
-    def __init__(self, config):
-        self.search = TavilySearch(max_results=2)
-        self.config = config
+class WebSearchTool(TavilySearch):
+    name: str = "web_search_tool"
+    description: str = (
+        "A search engine optimized for comprehensive, accurate, and trusted results. "
+        "Useful for when you need to answer questions about current events. "
+        "It not only retrieves URLs and snippets, but offers advanced search depths, "
+        "domain management, time range filters, and image search, this tool delivers "
+        "real-time, accurate, and citation-backed results."
+        "Input should be a search query."
+        "A Wrapper for the Tavily Search Tool to perform web searches. "
+        "Its difference is that it reads test data from a file in dev mode instead of making a web search. "
+    )
 
-    def searchWeb(self, query) -> str:
+    def invoke(self, config, query):
         search_results = None
 
-        if self.config.runtime_env == "prod":
-            search_results = self.search.invoke(query) 
+        if config.runtime_env == "prod":
+            search_results = super().invoke(query) 
 
-        elif self.config.runtime_env == "dev":
+        elif config.runtime_env == "dev":
             try:
-                with open(self.config.web_search_test_data_file, "r") as file:
+                with open(config.web_search_test_data_file, "r") as file:
                     search_results = str(json.load(file))
             except FileNotFoundError as e:
                 print(e)
