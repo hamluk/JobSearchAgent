bcd292eead090e1b63642a70ad99e6425e18ff26
Lukas Hamm <lukas@lukashamm.dev>
2025-08-02 17:57:51 +0800

    agent uses tavily search exclusivly, removes lines of code needed for the removed WebSearchAgent

--- src/job_search_agent/config.py
+++ src/job_search_agent/config.py
@@ -8,8 +8,5 @@ class Config(BaseSettings):
     llm_model: str
     model_provider: str
     croq_api_key: str
-    
-    runtime_env: str 
-    web_search_test_data_file: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
--- src/job_search_agent/job_search_agent.py
+++ src/job_search_agent/job_search_agent.py
@@ -1,16 +1,23 @@
+from langchain_tavily import TavilySearch
 from config import Config
 from langchain.chat_models import init_chat_model
-from web_search_agent import WebSearchTool
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 config = Config()
 
-search = WebSearchTool(max_results=1)
+memory = MemorySaver()
+memoryConfig = {"configurable": {"thread_id": "abc123"}}
+
+search = TavilySearch(max_results=1) 
 tools = [search] # Saving every tool in a list for later reference
 
 model = init_chat_model(config.llm_model, model_provider=config.model_provider, api_key=config.croq_api_key)
-model_with_tools = model.bind_tools(tools)
 
-response = model_with_tools.invoke([{"role": "user", "content": "What remote jobs for Software Engineers in Australia are available?"}])
+agent_executor = create_react_agent(model, tools, checkpointer=memory, max_iterations=2)
 
-print(f"Mesage content: {response.text()}\n")
-print(f"Tool calls: {response.tool_calls}")
+query = "What remote job applications are available for a Software Engineer in Australia?"
+
+input_message = {"role": "user", "content": query}
+for step in agent_executor.stream({"messages": [input_message]}, memoryConfig, stream_mode="values"):
+    step["messages"][-1].pretty_print()
